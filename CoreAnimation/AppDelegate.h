49ae3b68e2a4af14d13c8e9260ade001ab70ad1a
Eduardo Alvarado <e.alvaradod@gmail.com>
2016-02-08 16:40:41 -0600

    Initial Commit

---
+++ CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation
+//
+//  Created by Eduardo Alvarado Díaz on 2/8/16.
+//  Copyright © 2016 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnimation
+//
+//  Created by Eduardo Alvarado Díaz on 2/8/16.
+//  Copyright © 2016 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
